9cbd1ebee22cca49b2607c30db0eaf122570382d
tu-tang-jh <tu.tang@journeyh.io>
2024-08-07 10:27:30 +0700

    add modal sidebar

--- js/utils.js
+++ js/utils.js
@@ -1,21 +1,24 @@
-window.addEventListener("resize", () => {
-  const x = document.getElementById("hambuger-menu-links");
+// @ts-nocheck
+const hamburgerModal = document.getElementById("myModal");
+const hamburgerButton = document.getElementById("hamburger-menu");
+const closeHambugerButton = document.getElementsByClassName("close")[0];
 
-  if (!x) return;
+// When the user clicks the button, open the modal
+hamburgerButton.onclick = () => {
+  hamburgerModal.style.display = "block";
+};
 
-  if (window.innerWidth > 600) {
-    x.style.display = "none";
+// When the user clicks on <span> (x), close the modal
+closeHambugerButton.onclick = () => {
+  hamburgerModal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = (event) => {
+  if (
+    event.target == hamburgerModal ||
+    event.target.getAttribute("class") == "hamburger-menu-link"
+  ) {
+    hamburgerModal.style.display = "none";
   }
-});
-
-const handleClickHambugerButton = () => {
-  const x = document.getElementById("hambuger-menu-links");
-
-  if (!x) return;
-
-  if (x.style.display === "flex") {
-    x.style.display = "none";
-    return;
-  }
-  x.style.display = "flex";
 };
